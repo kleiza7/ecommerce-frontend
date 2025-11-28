02620ca266cb383c4f036fc8f192979467b249f6
Serafet <serafet6767@gmail.com>
2025-11-29 01:53:47 +0300

    rename repo

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,11 +1,11 @@
-import { NavLink } from 'react-router-dom';
-import CartPopover from './components/CartPopover';
+import { NavLink } from "react-router-dom";
+import CartPopover from "./components/CartPopover";
 
 const AppHeader = () => {
   return (
     <div className="h-20 bg-orange-500 justify-between shrink-0 flex items-center px-6">
       <NavLink to="/" className="text-s32-l40 font-bold text-white">
-        Shop Land
+        Ecommerce
       </NavLink>
       <CartPopover />
     </div>
