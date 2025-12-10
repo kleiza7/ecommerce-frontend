f4e238d1c662f37f3daa4d0e06524aee4ba8accf
Serafet <serafet6767@gmail.com>
2025-12-10 18:31:26 +0300

    feat:routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-function App() {
-  return (
-    <>
-      <div className="asdasd flex justify-between rounded-md bg-red-500 p-6 text-sm">
-        selamss
-      </div>
-    </>
-  );
-}
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
