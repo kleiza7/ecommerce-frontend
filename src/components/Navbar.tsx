83f3e3c8ec5bd884b6f6558530f0cbe5e722f2c9
Serafet <serafet6767@gmail.com>
2025-12-10 18:51:08 +0300

    fix:

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,9 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="bg-orange flex h-[100px] items-center px-6">
+      <span className="text-s48-l56 font-bold text-white">Ecommerce</span>
+    </header>
+  );
 };
 
 export default Navbar;
