eae0e9d9a6395918f6ca1378cb7743fddff185ea
Serafet <serafet6767@gmail.com>
2026-01-19 19:13:52 +0300

    fix:products forms jointed

--- src/pages/AdminProductsPage/components/ProductApprovalDialog/components/ProductApprovalForm.tsx
+++ src/pages/AdminProductsPage/components/ProductApprovalDialog/components/ProductApprovalForm.tsx
@@ -1,8 +1,5 @@
 import { useCallback, useEffect, useRef } from "react";
-import { Controller, useForm } from "react-hook-form";
-import type { ReqBrandsGetAllResponse } from "../../../../../api/responses/ReqBrandsGetAllResponse.model";
-import type { ReqCategoriesGetAllResponse } from "../../../../../api/responses/ReqCategoriesGetAllResponse.model";
-import type { ReqCurrenciesGetAllResponse } from "../../../../../api/responses/ReqCurrenciesGetAllResponse.model";
+import { useForm } from "react-hook-form";
 import type { ReqProductsGetByIdResponse } from "../../../../../api/responses/ReqProductsGetByIdResponse.model";
 import { useBrandsGetAll } from "../../../../../hooks/useBrandsGetAll";
 import { useCategoriesGetAll } from "../../../../../hooks/useCategoriesGetAll";
@@ -10,34 +7,18 @@ import { useCurrenciesGetAll } from "../../../../../hooks/useCurrenciesGetAll";
 import { useProductsApprove } from "../../../../../hooks/useProductsApprove";
 import { useProductsGetById } from "../../../../../hooks/useProductsGetById";
 import { useProductsReject } from "../../../../../hooks/useProductsReject";
-import GenericCategoryPicker from "../../../../../shared/components/GenericCategoryPicker";
 import {
   GenericDialogClose,
   GenericDialogTitle,
 } from "../../../../../shared/components/GenericDialog";
-import GenericFormInput from "../../../../../shared/components/GenericFormInput";
-import GenericFormTextArea from "../../../../../shared/components/GenericFormTextArea";
-import GenericImageInput from "../../../../../shared/components/GenericImageInput";
-import GenericSelect from "../../../../../shared/components/GenericSelect";
-import InputErrorLabel from "../../../../../shared/components/InputErrorLabel";
-import InputLabel from "../../../../../shared/components/InputLabel";
+import type { ProductFormType } from "../../../../../shared/components/ProductFormFields";
+import ProductFormFields from "../../../../../shared/components/ProductFormFields";
 import {
   BUTTON_ERROR,
   BUTTON_PRIMARY,
   BUTTON_PRIMARY_OUTLINED,
 } from "../../../../../shared/constants/CommonTailwindClasses.constants";
 import { customTwMerge } from "../../../../../shared/utils/Tailwind.util";
-
-type ProductApprovalFormType = {
-  name: string;
-  description: string;
-  stockCount: number;
-  price: number;
-  brand: ReqBrandsGetAllResponse[number];
-  category: ReqCategoriesGetAllResponse[number];
-  currency: ReqCurrenciesGetAllResponse[number];
-  images: File[];
-};
 
 const urlToFile = async (url: string, name: string) => {
   const res = await fetch(url);
@@ -70,7 +51,7 @@ const ProductApprovalForm = ({
     control,
     reset,
     formState: { errors },
-  } = useForm<ProductApprovalFormType>({
+  } = useForm<ProductFormType>({
     mode: "onChange",
     defaultValues: { images: [] },
   });
@@ -140,119 +121,13 @@ const ProductApprovalForm = ({
         </span>
       </div>
 
-      <div className="flex flex-col gap-y-5">
-        <div className="relative flex flex-col">
-          <InputLabel label="Name" hasAsterisk />
-          <GenericFormInput field="name" control={control} disabled />
-          <InputErrorLabel message={errors.name?.message} />
-        </div>
-
-        <div className="relative flex flex-col">
-          <InputLabel label="Description" hasAsterisk />
-          <GenericFormTextArea
-            field="description"
-            control={control}
-            rows={4}
-            disabled
-          />
-          <InputErrorLabel message={errors.description?.message} />
-        </div>
-
-        <div className="relative flex flex-col">
-          <InputLabel label="Brand" hasAsterisk />
-          <Controller
-            name="brand"
-            control={control}
-            render={({ field }) => (
-              <GenericSelect
-                value={field.value}
-                options={brands.map((brand) => ({
-                  label: brand.name,
-                  value: brand,
-                }))}
-                onChange={field.onChange}
-                disabled
-              />
-            )}
-          />
-          <InputErrorLabel message={errors.brand?.message} />
-        </div>
-
-        <div className="relative flex flex-col">
-          <InputLabel label="Category" hasAsterisk />
-
-          <Controller
-            name="category"
-            control={control}
-            render={({ field }) => (
-              <GenericCategoryPicker
-                value={field.value}
-                onChange={field.onChange}
-                disabled
-              />
-            )}
-          />
-
-          <InputErrorLabel message={errors.category?.message} />
-        </div>
-
-        <div className="relative flex flex-col">
-          <InputLabel label="Stock Count" hasAsterisk />
-          <GenericFormInput
-            field="stockCount"
-            control={control}
-            type="number"
-            disabled
-          />
-          <InputErrorLabel message={errors.stockCount?.message} />
-        </div>
-
-        <div className="flex gap-x-4">
-          <div className="relative flex flex-1 flex-col">
-            <InputLabel label="Price" hasAsterisk />
-            <GenericFormInput
-              field="price"
-              control={control}
-              type="number"
-              disabled
-            />
-            <InputErrorLabel message={errors.price?.message} />
-          </div>
-
-          <div className="relative flex flex-1 flex-col">
-            <InputLabel label="Currency" hasAsterisk />
-            <Controller
-              name="currency"
-              control={control}
-              render={({ field }) => (
-                <GenericSelect
-                  value={field.value}
-                  options={currencies.map((currency) => ({
-                    label: currency.code,
-                    value: currency,
-                  }))}
-                  onChange={field.onChange}
-                  disabled
-                />
-              )}
-            />
-            <InputErrorLabel message={errors.currency?.message} />
-          </div>
-        </div>
-
-        <div className="relative flex flex-col">
-          <InputLabel label="Images" hasAsterisk />
-
-          <GenericImageInput
-            field="images"
-            control={control}
-            disabled
-            exactFileCount={4}
-          />
-
-          <InputErrorLabel message={errors.images?.message} />
-        </div>
-      </div>
+      <ProductFormFields
+        control={control}
+        errors={errors}
+        brands={brands}
+        currencies={currencies}
+        disabled
+      />
 
       <div className="flex justify-end gap-x-2">
         <GenericDialogClose>
