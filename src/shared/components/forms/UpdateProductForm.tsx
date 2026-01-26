ed7b02c3464799d61e86561388c7df2c66f3fc7a
Serafet <serafet6767@gmail.com>
2026-01-26 15:14:06 +0300

    fix:product form fields removed

--- src/shared/components/forms/UpdateProductForm.tsx
+++ src/shared/components/forms/UpdateProductForm.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useRef } from "react";
-import { useForm, type SubmitHandler } from "react-hook-form";
+import { Controller, useForm, type SubmitHandler } from "react-hook-form";
+import type { ReqBrandsGetAllResponse } from "../../../api/responses/ReqBrandsGetAllResponse.model";
+import type { ReqCategoriesGetAllResponse } from "../../../api/responses/ReqCategoriesGetAllResponse.model";
+import type { ReqCurrenciesGetAllResponse } from "../../../api/responses/ReqCurrenciesGetAllResponse.model";
 import type { ReqProductsGetByIdResponse } from "../../../api/responses/ReqProductsGetByIdResponse.model";
 import { useBrandsGetAll } from "../../../hooks/useBrandsGetAll";
 import { useCategoriesGetAll } from "../../../hooks/useCategoriesGetAll";
@@ -11,9 +14,25 @@ import {
   BUTTON_PRIMARY_OUTLINED,
 } from "../../constants/CommonTailwindClasses.constants";
 import { customTwMerge } from "../../utils/Tailwind.util";
+import GenericCategoryPicker from "../GenericCategoryPicker/GenericCategoryPicker";
 import { GenericDialogClose, GenericDialogTitle } from "../GenericDialog";
-import type { ProductFormType } from "../ProductFormFields";
-import ProductFormFields from "../ProductFormFields";
+import GenericFormInput from "../GenericFormInput";
+import GenericFormTextArea from "../GenericFormTextArea";
+import GenericImageInput from "../GenericImageInput";
+import GenericSelect from "../GenericSelect";
+import InputErrorLabel from "../InputErrorLabel";
+import InputLabel from "../InputLabel";
+
+type UpdateProductFormType = {
+  name: string;
+  description: string;
+  stockCount: number;
+  price: number;
+  brand: ReqBrandsGetAllResponse[number];
+  category: ReqCategoriesGetAllResponse[number];
+  currency: ReqCurrenciesGetAllResponse[number];
+  images: File[];
+};
 
 const urlToFile = async (url: string, name: string) => {
   const res = await fetch(url);
@@ -42,12 +61,12 @@ const UpdateProductForm = ({
     handleSubmit,
     reset,
     formState: { isValid, errors },
-  } = useForm<ProductFormType>({
+  } = useForm<UpdateProductFormType>({
     mode: "onChange",
     defaultValues: { images: [] },
   });
 
-  const onSubmit: SubmitHandler<ProductFormType> = (values) => {
+  const onSubmit: SubmitHandler<UpdateProductFormType> = (values) => {
     const originalImages = productRef.current?.images ?? [];
 
     const deletedImageIds = originalImages
@@ -137,13 +156,145 @@ const UpdateProductForm = ({
       </div>
 
       <div className="flex-1 overflow-y-auto">
-        <ProductFormFields
-          control={control}
-          errors={errors}
-          brands={brands}
-          currencies={currencies}
-          disabled={isPending}
-        />
+        <div className="flex flex-col gap-y-5">
+          <div className="relative flex flex-col">
+            <InputLabel label="Name" hasAsterisk />
+
+            <GenericFormInput
+              field="name"
+              control={control}
+              required
+              disabled={isPending}
+            />
+
+            <InputErrorLabel message={errors.name?.message} />
+          </div>
+
+          <div className="relative flex flex-col">
+            <InputLabel label="Description" hasAsterisk />
+
+            <GenericFormTextArea
+              field="description"
+              control={control}
+              required
+              rows={4}
+              disabled={isPending}
+            />
+            <InputErrorLabel message={errors.description?.message} />
+          </div>
+
+          <div className="relative flex flex-col">
+            <InputLabel label="Brand" hasAsterisk />
+
+            <Controller
+              name="brand"
+              control={control}
+              rules={{ required: "Brand is required!" }}
+              render={({ field }) => (
+                <GenericSelect
+                  placeholder="Select brand"
+                  value={field.value}
+                  options={brands.map((brand) => ({
+                    label: brand.name,
+                    value: brand,
+                  }))}
+                  onChange={field.onChange}
+                  disabled={isPending}
+                />
+              )}
+            />
+
+            <InputErrorLabel message={errors.brand?.message} />
+          </div>
+
+          <div className="relative flex flex-col">
+            <InputLabel label="Category" hasAsterisk />
+
+            <Controller
+              name="category"
+              control={control}
+              rules={{ required: "Category is required!" }}
+              render={({ field }) => (
+                <GenericCategoryPicker
+                  value={field.value}
+                  onChange={field.onChange}
+                  disabled={isPending}
+                />
+              )}
+            />
+
+            <InputErrorLabel message={errors.category?.message} />
+          </div>
+
+          <div className="relative flex flex-col">
+            <InputLabel label="Stock Count" hasAsterisk />
+
+            <GenericFormInput
+              field="stockCount"
+              control={control}
+              required
+              type="number"
+              min={1}
+              disabled={isPending}
+            />
+
+            <InputErrorLabel message={errors.stockCount?.message} />
+          </div>
+
+          <div className="flex gap-x-4">
+            <div className="relative flex flex-1 flex-col">
+              <InputLabel label="Price" hasAsterisk />
+
+              <GenericFormInput
+                field="price"
+                control={control}
+                required
+                type="number"
+                min={1}
+                disabled={isPending}
+              />
+
+              <InputErrorLabel message={errors.price?.message} />
+            </div>
+
+            <div className="relative flex flex-1 flex-col">
+              <InputLabel label="Currency" hasAsterisk />
+
+              <Controller
+                name="currency"
+                control={control}
+                rules={{ required: "Currency is required!" }}
+                render={({ field }) => (
+                  <GenericSelect
+                    placeholder="Select currency"
+                    value={field.value}
+                    options={currencies.map((currency) => ({
+                      label: currency.code,
+                      value: currency,
+                    }))}
+                    onChange={field.onChange}
+                    disabled={isPending}
+                  />
+                )}
+              />
+
+              <InputErrorLabel message={errors.currency?.message} />
+            </div>
+          </div>
+
+          <div className="relative flex flex-col">
+            <InputLabel label="Images" hasAsterisk />
+
+            <GenericImageInput
+              field="images"
+              control={control}
+              disabled={isPending}
+              exactFileCount={4}
+            />
+
+            <InputErrorLabel message={errors.images?.message} />
+          </div>
+        </div>
       </div>
 
       <div className="flex shrink-0 justify-end gap-x-2">
