eebed375cb2c71347fef4fb7079294404859a57e
Serafet <serafet6767@gmail.com>
2026-03-02 18:24:45 +0300

    fix:

--- src/pages/ProductDetailPage/components/ProductDetailTabs/components/AboutSellerSection.tsx
+++ src/pages/ProductDetailPage/components/ProductDetailTabs/components/AboutSellerSection.tsx
@@ -85,8 +85,8 @@ const AboutSellerSection = ({
     <div className="border-border-primary flex flex-col rounded-xl border">
       <div className="border-border-primary flex flex-col gap-y-4 border-b p-8 sm:flex-row sm:items-center sm:justify-between sm:gap-y-0">
         <div className="flex items-center gap-x-4">
-          <div className="bg-status-info-muted border-status-info-secondary flex h-20 w-20 shrink-0 items-center justify-center rounded-full border">
-            <StoreFrontIcon className="fill-status-info-primary h-14 w-14" />
+          <div className="bg-status-info-muted border-status-info-secondary flex h-12 w-12 shrink-0 items-center justify-center rounded-full border sm:h-20 sm:w-20">
+            <StoreFrontIcon className="fill-status-info-primary h-8 w-8 sm:h-14 sm:w-14" />
           </div>
 
           <div className="flex flex-col">
